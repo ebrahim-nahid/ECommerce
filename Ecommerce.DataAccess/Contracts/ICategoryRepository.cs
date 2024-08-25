7ff5d22506b74ebf69981e34c4f8510baca271ae
ebrahim-nahid <mdebrahim1704@gmail.com>
2024-08-25 19:41:30 +0600

    database creation create and get feature done

---
+++ Ecommerce.DataAccess/Contracts/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Ecommerce.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.Contracts;
+
+public interface ICategoryRepository
+{
+    Task<bool> Create(Category category);
+    Task<IReadOnlyList<Category>> Get();
+    Task<Category> Get(int id);
+    Task<bool> Update(Category category);
+    Task<bool> Delete(int id);
+}
